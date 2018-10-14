105af2c0badd127077f59b1770f396c563a0213e
Janes <Janes@DESKTOP-OORTNHE>
2018-10-14 18:05:25 +0200

    -Gui now in own thread
    -File -> Open - handler implemented

--- PIC16F84-Simulator/src/Main.java
+++ PIC16F84-Simulator/src/Main.java
@@ -1,5 +1,16 @@
+import gui.GUI_Main;
 
 public class Main
 {
-	//Test 
+	static GUI_Main gui;
+	
+	public static void main(String args[])
+	{
+		
+		gui = new GUI_Main();
+		Thread guiThread = new Thread(gui);
+		guiThread.start();
+		
+	}
+	
 }
--- PIC16F84-Simulator/src/gui/GUI_Main.java
+++ PIC16F84-Simulator/src/gui/GUI_Main.java
@@ -1,38 +1,59 @@
 package gui;
 
+import java.util.Map;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 
 
-public class GUI_Main extends Application 
+public class GUI_Main extends Application implements Runnable
 {
-	 @Override
-	    public void start(Stage stage) throws Exception {
-	        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
-	         
-	        Scene scene = new Scene(root);
+	@Override
+    public void start(Stage stage) throws Exception 
+	{
+	 	FXMLLoader loader = new FXMLLoader(getClass().getResource("layout.fxml"));
+	    Parent root = loader.load();
+	    
+	    Map<String, Object> namespace = loader.getNamespace();
 
-	        stage.setScene(scene);
-	        stage.show();
-	        
-	        /*
-	        Button btn = (Button)scene.lookup("#btnTest");
-	        
-	        if(btn != null)
-	        	btn.setText("TEEEEEST!");
-	        else 
-	        	System.out.println("NULL");
-	        */
-	        // nicer kommentar
-	    }
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        
+        setup(scene, namespace);
+    }
+
+	@Override
+	public void run()
+	{
+		launch();
+	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	private void setup(Scene scene, Map<String, Object> namespace)
+	{
+	    MenuItem open = (MenuItem) namespace.get("menuItem_Open"); //fx:id
 		
+	    //TODO set up event handlers 
+		if(open != null)
+			open.setOnAction(event -> this.onOpenDocument());
+		else
+			System.out.println("Not found");
 		
 	}
+
+	private Object onOpenDocument()
+	{
+		//TODO Open a file
+		System.out.println("Open clicked!");
+		return null;
+	}
+	
+	
+	
+	
 }
