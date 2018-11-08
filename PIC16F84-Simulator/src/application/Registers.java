ed09b238c90e05c7eb2320996e4e3dfa654679c6
Janes <Janes@Janes-Laptop>
2018-11-08 12:05:08 +0100

    gui - registers in right pane

--- PIC16F84-Simulator/src/application/Registers.java
+++ PIC16F84-Simulator/src/application/Registers.java
@@ -5,8 +5,8 @@ public class Registers
 	//int[] Indirect_Addr, TMR0, PCL, STATUS, FSR, PORTA, PORTB, EEDATA, EEADR, PCLATH, INTCON = new int[8];	//Bank0
 	//int[] Indirect_Addr, OPTION, PCL, STATUS, FSR, TRISA, TRISB, EECON1, EECON2, PCLATH, INTCON = new int[8];	//Bank1
 	
-	private int bank0[] = new int[80];
-	private int bank1[] = new int[80];
+	private int bank0[] = new int[128];
+	private int bank1[] = new int[128];
 	
 	// Properties
 	private int working;
